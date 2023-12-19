6d9b93b1fa42cff72d0c978b5c4bd2fad9b8709e
MiraRay13 <137090329+MiraRay13@users.noreply.github.com>
2023-12-19 01:44:04 +0000

    added first task

---
+++ hello.py
@@ -0,0 +1,11 @@
+# Get user input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+college = input("Enter your college: ")
+profession = input("Enter your profession: ")
+pet_name = input("Enter your pet's name: ")
+
+story = f"There once was a person named {name} who lived in {city}. At the age of {age}, {name} went to college at {college}. {name} graduated and went to work as a {profession}. Then, {name} adopted an animal named {pet_name}. They both lived happily ever after."
+
+print(story)
